101bc2524f79d1609fb4fd696b6eb99698a159ff
fibrili <55363976+finerbrighterlighter@users.noreply.github.com>
2021-05-14 10:12:53 +0700

    update Y class
    update class (Y) every outer loop.
    update logit (y) afterwards.
    sorry to do multiple pulls. I missed a couple lines.

--- R/MixRFb.r
+++ R/MixRFb.r
@@ -108,15 +108,15 @@ MixRFb <- function(Y, x, random, data, initialRandomEffects=0,
       if(verbose) print(c(iterations0,iterations,LogLik))
     }
 
-    eta = pred + AllEffects
-    mu = 1/(1+exp(-eta))
-    y = eta + (Y-mu)/(mu*(1-mu))
-    AdjustedTarget <- y - AllEffects
-    weights = as.vector(mu*(1-mu))
+    neweta  <-pred + AllEffects
+    newmu <- 1/(1+exp(-neweta))
+    newY <- (newmu>=0.35)
+    newy <- eta + (newY-newmu)/(newmu*(1-newmu))
+    weights = as.vector(newmu*(1-newmu))
 
-    print(c(iter = iterations0, maxEtaChange=max(abs(eta-oldEta))))
+    print(c(iter = iterations0, maxEtaChange=max(abs(neweta-oldEta))))
 
-    ContinueCondition0 <- (max(abs(eta-oldEta))>ErrorTolerance0 & iterations0 < MaxIterations0)
+    ContinueCondition0 <- (max(abs(neweta-oldEta))>ErrorTolerance0 & iterations0 < MaxIterations0)
     oldEta <- eta
   }
 
